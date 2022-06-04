ab11cc4cba801d104aaa95df541bc96fa6cb2b64
emyrue <94935633+emyrue@users.noreply.github.com>
2022-06-04 13:20:13 -0600

    Photos added

--- src/allphotos.js
+++ src/allphotos.js
@@ -20,6 +20,72 @@ import image19 from './photos/image19.png';
 import image20 from './photos/image20.png';
 import image21 from './photos/image21.png';
 import image22 from './photos/image22.png';
+import image23 from './photos/image23.png';
+import image24 from './photos/image24.png';
+import image25 from './photos/image25.png';
+import image26 from './photos/image26.png';
+import image27 from './photos/image27.png';
+import image28 from './photos/image28.png';
+import image29 from './photos/image29.png';
+import image30 from './photos/image30.png';
+import image31 from './photos/image31.png';
+import image32 from './photos/image32.png';
+import image33 from './photos/image33.png';
+import image34 from './photos/image34.png';
+import image35 from './photos/image35.png';
+import image36 from './photos/image36.png';
+import image37 from './photos/image37.png';
+import image38 from './photos/image38.png';
+import image39 from './photos/image39.png';
+import image40 from './photos/image40.png';
+import image41 from './photos/image41.png';
+import image42 from './photos/image42.png';
+import image43 from './photos/image43.png';
+import image44 from './photos/image44.png';
+import image45 from './photos/image45.png';
+import image46 from './photos/image46.png';
+import image47 from './photos/image47.png';
+import image48 from './photos/image48.png';
+import image49 from './photos/image49.png';
+import image50 from './photos/image50.png';
+import image51 from './photos/image51.png';
+import image52 from './photos/image52.png';
+import image53 from './photos/image53.png';
+import image54 from './photos/image54.png';
+import image55 from './photos/image55.png';
+import image56 from './photos/image56.png';
+import image57 from './photos/image57.png';
+import image58 from './photos/image58.png';
+import image59 from './photos/image59.png';
+import image60 from './photos/image60.png';
+import image61 from './photos/image61.png';
+import image62 from './photos/image62.png';
+import image63 from './photos/image63.png';
+import image64 from './photos/image64.png';
+import image65 from './photos/image65.png';
+import image66 from './photos/image66.png';
+import image67 from './photos/image67.png';
+import image68 from './photos/image68.png';
+import image69 from './photos/image69.png';
+import image70 from './photos/image70.png';
+import image71 from './photos/image71.png';
+import image72 from './photos/image72.png';
+import image73 from './photos/image73.png';
+import image74 from './photos/image74.png';
+import image75 from './photos/image75.png';
+import image76 from './photos/image76.png';
+import image77 from './photos/image77.png';
+import image78 from './photos/image78.png';
+import image79 from './photos/image79.png';
+import image80 from './photos/image80.png';
+import image81 from './photos/image81.png';
+import image82 from './photos/image82.png';
+import image83 from './photos/image83.png';
+import image84 from './photos/image84.png';
+import image85 from './photos/image85.png';
+import image86 from './photos/image86.png';
+import image87 from './photos/image87.png';
+import image88 from './photos/image88.png';
 
 const images = [
   image1,
@@ -44,6 +110,72 @@ const images = [
   image20,
   image21,
   image22,
+  image23,
+  image24,
+  image25,
+  image26,
+  image27,
+  image28,
+  image29,
+  image30,
+  image31,
+  image32,
+  image33,
+  image34,
+  image35,
+  image36,
+  image37,
+  image38,
+  image39,
+  image40,
+  image41,
+  image42,
+  image43,
+  image44,
+  image45,
+  image46,
+  image47,
+  image48,
+  image49,
+  image50,
+  image51,
+  image52,
+  image53,
+  image54,
+  image55,
+  image56,
+  image57,
+  image58,
+  image59,
+  image60,
+  image61,
+  image62,
+  image63,
+  image64,
+  image65,
+  image66,
+  image67,
+  image68,
+  image69,
+  image70,
+  image71,
+  image72,
+  image73,
+  image74,
+  image75,
+  image76,
+  image77,
+  image78,
+  image79,
+  image80,
+  image81,
+  image82,
+  image83,
+  image84,
+  image85,
+  image86,
+  image87,
+  image88,
 ];
 
 export default images;

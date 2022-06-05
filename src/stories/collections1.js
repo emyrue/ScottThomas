e7b858da21d52f0403fd7649b94723d95eddd4aa
emyrue <94935633+emyrue@users.noreply.github.com>
2022-06-04 20:09:32 -0600

    Fix contribute page

--- src/stories/collections1.js
+++ src/stories/collections1.js
@@ -1,11 +1,11 @@
-import image89 from '../story-photos/image89.png';
+import storyImage1 from '../story-photos/storyImage1.png';
 
 const collection1 = {
   name: 'Emily Robertson',
   id: 'emilyrobertson',
   href: '#emilyrobertson',
   images: [
-    image89,
+    storyImage1,
   ],
   stories: [
     'Grandpa Scott took me out to see Chitty Chitty Bang Bang at the Hale Center Theater. We really enjoyed the show, and at the end, I asked Grandpa to stand behind the Chitty Chitty Bang Bang cutout and took this picture of him, which I will treasure forever. I love the time that I spend with my Grandpa. He really loves his grandkids, and you can see it in everything he does.',
